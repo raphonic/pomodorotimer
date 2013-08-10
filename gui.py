c283b6aca0abe8b5b45e861f79367631df7db7a7
raphonic <raphonic@outlook.com>
2013-08-10 14:19:22 +0530

    Basic functionality is now working, need to add the logs functionality and also refactor the code

--- gui.py
+++ gui.py
@@ -6,39 +6,39 @@ class MainFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, wx.ID_ANY, "Pomodoro", 
                                    size=(200,200))
- 
-        panel = wx.Panel(self, wx.ID_ANY)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-
+        self.panel = wx.Panel(self, wx.ID_ANY)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.Center()
-
         self.initialize_timers()
-        
-        self.timer_label = wx.StaticText(panel, wx.ID_ANY)
-        font = wx.Font(40, wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
-        self.timer_label.SetFont(font)
-
-        self.start_btn = GB.GradientButton(panel, wx.ID_ANY, label="Start")
-        self.start_btn.Bind(wx.EVT_BUTTON, self.on_start)
-
-        self.cancel_btn = GB.GradientButton(panel, wx.ID_ANY, label="Cancel")
-        self.cancel_btn.Bind(wx.EVT_BUTTON, self.on_cancel)
-        
-        sizer.Add(self.timer_label, flag = wx.ALL, border=10)
-        
-        hsizer = wx.BoxSizer(wx.HORIZONTAL)
-        hsizer.Add(self.start_btn, flag=wx.ALL, border=5)
-        hsizer.Add(self.cancel_btn, flag=wx.ALL, border=5) 
-        sizer.Add(hsizer)
-
-        panel.SetSizer(sizer, wx.ALL)
-
+        self.initialize_timer_label()
+        self.initialize_buttons()
+        self.set_sizers()
         self.set_defaults()
 
     def initialize_timers(self):
         self.pomodoro_timer = Pomodoro(self)
-        self.Bind(wx.EVT_TIMER, self.update, self.pomodoro_timer)
+        self.break_timer = Break(self)
+        self.current_timer = self.pomodoro_timer
+        
+    def initialize_timer_label(self):
+        self.timer_label = wx.StaticText(self.panel, wx.ID_ANY)
+        font = wx.Font(40, wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
+        self.timer_label.SetFont(font)
 
+    def initialize_buttons(self):
+        self.start_btn = GB.GradientButton(self.panel, wx.ID_ANY, label="Start")
+        self.start_btn.Bind(wx.EVT_BUTTON, self.on_start)
+        self.cancel_btn = GB.GradientButton(self.panel, wx.ID_ANY, label="Cancel")
+        self.cancel_btn.Bind(wx.EVT_BUTTON, self.on_cancel)
+
+    def set_sizers(self):
+        self.sizer.Add(self.timer_label, flag = wx.ALL, border=10)
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer.Add(self.start_btn, flag=wx.ALL, border=5)
+        hsizer.Add(self.cancel_btn, flag=wx.ALL, border=5) 
+        self.sizer.Add(hsizer)
+        self.panel.SetSizer(self.sizer, wx.ALL)
+        
     def on_start(self, event):
         btnLabel = self.start_btn.GetLabel()
         if btnLabel == "Start":
@@ -48,37 +48,16 @@ class MainFrame(wx.Frame):
             self.pomodoro_timer.Stop()
             self.start_btn.SetLabel("Start")
 
-    def on_pomodoro_completed(self):
-        self.pomodoro_timer.Stop()
-        self.pomodoro_timer.mark_as_complete()
-        self.pomodoro_timer.reset()
-        self.set_defaults()
-        self.start_break()
-
-    def start_break(self):
-        self.pomodoro_timer.Start()
-
     def set_defaults(self):
         self.start_btn.SetLabel("Start")
-        self.timer_label.SetLabel(self.pomodoro_timer.formatted_time_left()) 
+        self.timer_label.SetLabel(self.current_timer.formatted_time_left()) 
         self.Refresh()
 
     def on_cancel(self, event):
         self.pomodoro_timer.reset()        
-        self.pomodoro_timer.Stop()
         self.start_btn.SetLabel("Start")        
         self.set_defaults()
         self.Refresh()
-
-    def update_pomodoro(self):
-        self.timer_label.SetLabel(self.pomodoro_timer.formatted_time_left()) 
-        
-    def update(self, event):
-        if self.pomodoro_timer.is_complete():
-            self.on_pomodoro_completed()
-        else:
-            self.pomodoro_timer.decrement_one_sec()
-            self.update_pomodoro()
 
 if __name__ == "__main__":
     app = wx.PySimpleApp()
--- pomodoro.py
+++ pomodoro.py
@@ -5,13 +5,16 @@ class TimeDuration(wx.Timer):
     
     def __init__(self, parent):
         wx.Timer.__init__(self,parent)
+        self.parent = parent
         self.completed = False
         self.time_left = self.Duration
+        self.parent.Bind(wx.EVT_TIMER, self.on_update, self)
 
     def decrement_one_sec(self):
         self.time_left -= 1
 
     def reset(self):
+        self.Stop()
         self.time_left = self.Duration
 
     def mark_as_complete(self):
@@ -23,15 +26,36 @@ class TimeDuration(wx.Timer):
     def formatted_time_left(self):
         hours = self.time_left / 60
         mins = self.time_left % 60
-        if mins == 0: 
-            return str(hours) + ":" + str(mins) + "0"
+        fmt_str = str(hours) + ":" + str(mins)
+        if mins == 0:
+            return fmt_str  + "0"
         else:
-            return str(hours) + ":" + str(mins)
+            return fmt_str
+
+    def on_update(self, event):
+        if self.is_complete():
+            self.on_completed()
+        else:
+            self.decrement_one_sec()
+            self.parent.timer_label.SetLabel(self.formatted_time_left())
+
+    def on_completed(self):
+        self.reset()
+        self.mark_as_complete()
 
 class Pomodoro(TimeDuration):
     Duration = 3
 
+    def on_completed(self):
+        TimeDuration.on_completed(self)
+        self.parent.break_timer.Start()
+        self.parent.current_timer = self.parent.break_timer
+        self.parent.set_defaults()
+
 class Break(TimeDuration):
     Duration = 2
 
-    
+    def on_completed(self):
+        TimeDuration.on_completed(self)
+        self.parent.current_timer = self.parent.pomodoro_timer
+        self.parent.set_defaults()
